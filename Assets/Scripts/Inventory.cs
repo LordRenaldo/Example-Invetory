fb913e4125020839222bbc8f028b67c936555fe5
Renaldo <estudiante11051987@outlook.com>
2026-02-07 00:05:21 -0300

    Feat: add  Inventory Class and Item Class

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+    public List<ItemData> ItemList;
+    public Inventory ()
+    {
+        ItemList = new List<ItemData> ();
+
+        AddItem (new ItemData { itemType = ItemData.Itemtype.HealthPotion, Amount = 1 });
+
+        Debug.Log (ItemList.Count);
+    }
+
+    public void AddItem ( ItemData item )
+    {
+        if (item != null) ItemList.Add (item);
+    }
+
+    public void RemoveItem ( ItemData item )
+    {
+        if (ItemList.Contains (item)) ItemList.Remove (item);
+    }
+
+
+}
---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ItemData 
+{
+    public enum Itemtype { HealthPotion,ManaPotion,Sword,Coin,Stone,Ring }
+    public Itemtype itemType;
+    public int Amount;
+}
